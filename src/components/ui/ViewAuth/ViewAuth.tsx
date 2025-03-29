d200e02f5929193673e963b0a7978b80201baf9d
polyakov-artem <polyakov.contact@yandex.ru>
2025-03-29 13:58:33 +0300

    feat: add redirection of users to the main page upon successful account creation or login

--- src/components/ui/ViewAuth/ViewAuth.tsx
+++ src/components/ui/ViewAuth/ViewAuth.tsx
@@ -1,6 +1,12 @@
 import { FC } from 'react';
 import AuthWindow from '../AuthWindow/AuthWindow';
 import { TAuthProps, TIntrinsicMain } from '../../../types/types';
+import { useAppSelector } from '../../../hooks/store-hooks';
+import { Navigate, useLocation } from 'react-router';
+import { selectUserRole } from '../../../store/authSlice';
+import { TokenRole } from '../../../services/authService';
+import { getFullPath } from '../../../utils/getFullPath';
+import { VIEW_MAIN } from '../../../routes';
 import './ViewAuth.scss';
 
 export const VIEW_AUTH = 'view-auth';
@@ -9,6 +15,14 @@ export const VIEW_AUTH_WINDOW = `${VIEW_AUTH}__window`;
 export type TAuthViewProps = TIntrinsicMain & TAuthProps;
 
 const ViewAuth: FC<TAuthViewProps> = ({ type }) => {
+  const role = useAppSelector(selectUserRole);
+  const location = useLocation();
+
+  if (role === TokenRole.user) {
+    const from = (location as { state?: { from?: { pathname?: string } } }).state?.from?.pathname;
+    return <Navigate to={from ? from : getFullPath(VIEW_MAIN)} relative="path" replace />;
+  }
+
   return (
     <main className={VIEW_AUTH}>
       <AuthWindow className={VIEW_AUTH_WINDOW} type={type} />
